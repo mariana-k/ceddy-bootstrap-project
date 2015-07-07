e62c1d7789f64a89b971c05f90646435c484478e
Mariana Kyrkosh <mariana.kyrkosh@gmail.com>
2015-07-07 15:02:48 +0200

    first commit

---
+++ app/scripts/main.js
@@ -0,0 +1,36 @@
+/* datepicker */
+$('.datepicker').datepicker({});
+
+/* multiple images carousel */
+(function($) {
+    $(function() {
+        var jcarousel = $('.jcarousel');
+
+        jcarousel
+            .on('jcarousel:reload jcarousel:create', function () {
+                var carousel = $(this),
+                    width = carousel.innerWidth();
+
+                if (width >= 600) {
+                    width = width / 3;
+                } else if (width >= 350) {
+                    width = width / 2;
+                }
+
+                carousel.jcarousel('items').css('width', Math.ceil(width) + 'px');
+            })
+            .jcarousel({
+                wrap: 'circular'
+            });
+
+        $('.jcarousel-control-prev')
+            .jcarouselControl({
+                target: '-=1'
+            });
+
+        $('.jcarousel-control-next')
+            .jcarouselControl({
+                target: '+=1'
+            });
+    });
+})(jQuery);
